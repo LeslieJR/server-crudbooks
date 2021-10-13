97654efe03f8be84c911bb012a49303c3cff5dc6
LeslieJR <l.reyes.ubiqum@gmail.com>
2021-10-13 22:31:27 +0200

    make it better

--- api/mongo.js
+++ api/mongo.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-const dbname = 'crud-books'
+const uri = process.env.MONGODB_URI;
+const options = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  }
 
-mongoose.connect('mongodb://localhost/'+dbname)
+mongoose.connect(uri, options)
 .then(()=>{
  console.log('the connection was successful') 
 })

a17a32a93e5d05e39cdea50a56471381fed816ad
LeslieJR <l.reyes.ubiqum@gmail.com>
2021-10-14 11:58:22 +0200

    mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 const mongo = require("./mongo");
 const server = require("./server");
 
-server.set("port", 4000);
-server.listen(process.env.PORT || server.get("port"), () => {
-  console.log("listening to port number: ", server.get("port"));
-});
-
-async (arg1, arg2, arg3) => {
+async () => {
   await mongo().then(async (mongoose) => {
     try {
-      console.log("Connected to mongo!!");
+      console.log("Connected to mongo");
       await command.execute(client, message, args);
     } finally {
       mongoose.connection.close();
     }
   });
 };
+
+server.set("port", 4000);
+server.listen(process.env.PORT || server.get("port"), () => {
+  console.log("listening to port number: ", server.get("port"));
+});
